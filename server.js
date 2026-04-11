b6bea1dc5c0bca79943524ded94693cd3b5544bd
Olga Shabak <olgashabak@gmail.com>
2026-04-11 19:59:28 +0300

    Fix port for deployment

--- server.js
+++ server.js
@@ -261,6 +261,8 @@ setInterval(async () => {
 }, 60000);
 
 // запуск сервера
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
